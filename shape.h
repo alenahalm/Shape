5ff3357f8d59ac825ad49a6c4f63a6ef7147ffff
alenahalm <75882124+alenahalm@users.noreply.github.com>
2021-11-20 17:47:04 +0800

    Add files via upload

--- shape.h
+++ shape.h
@@ -3,7 +3,25 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
+
+class Point {
+private:
+	double x, y;
+public:
+	Point(double x, double y) {
+		this->x = x;
+		this->y = y;
+	}
+	double get_X() {
+		return x;
+	}
+	double get_Y() {
+		return y;
+	}
+	
+};
 
 class Shape {
 public:
@@ -73,10 +91,37 @@ public:
 
 class Polygon : public Shape {
 private:
-	vector <pair> points;
+	vector <Point> points;
 public:
-	Polygon(vector <pair> p) {
+	Polygon(vector <Point> p) {
 		this->points = p;
+	}
+	double area() {
+		double s = 0;
+		for (int i = 0; i < points.size(); i++) {
+			if (i < points.size() - 1) {
+				s += points[i].get_X() * points[i + 1].get_Y();
+				s -= points[i].get_Y() * points[i + 1].get_X();
+			}
+			else {
+				s += points[i].get_X() * points[0].get_Y();
+				s -= points[i].get_Y() * points[0].get_X();
+			}
+		}
+		return abs(s * 0.5);
+	}
+	double perimeter() {
+		double p = 0;
+		for (int i = 0; i < points.size() - 1; i++) {
+			double x, y;
+			x = points[i + 1].get_X() - points[i].get_X();
+			y = points[i + 1].get_Y() - points[i].get_Y();
+			p += sqrt(x * x + y * y);
+		}
+		return p;
+	}
+	void my_shape() {
+		cout << "Polygon";
 	}
 };
 #endif // !shape
--- shapes.cpp
+++ shapes.cpp
@@ -5,5 +5,14 @@ using namespace std;
 
 int main()
 {
-	
+	vector<Point> v;
+
+	for (int i = 0; i < 5; i++) {
+		double x, y;
+		cin >> x >> y;
+		Point p(x, y);
+		v.push_back(p);
+	}
+	Polygon sh(v);
+	cout << sh.area() << endl;
 }
